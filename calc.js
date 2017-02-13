172a4c36fc3dd0c4e11edc70e0160c3b0479bdc8
Staphsynth <1337dallen@gmail.com>
2017-02-13 14:18:40 +1100

    start re-factor to use strings for display instead of floats

--- calc.js
+++ calc.js
@@ -2,54 +2,64 @@ var calcApp = angular.module('calc-app', []);
 
 calcApp.controller('calc-controller', function($scope){
   //globals
-  $scope.display = 0;
-  $scope.operand = 0;
+  $scope.operand1 = '0';
+  $scope.operand2 = '0';
+  $scope.minus = false;
   $scope.decPoint = false;
-  $scope.postDecPresses = 0;
   $scope.operator = '';
   $scope.buttons = [
-    [':)',';)',':D','C'],
-    [7,8,9,'+'],
-    [4,5,6,'-'],
-    [1,2,3,'/'],
-    ['.',0,'=','*']
+    [':)',':D','±','C'],
+    ['7','8','9','+'],
+    ['4','5','6','-'],
+    ['1','2','3','/'],
+    ['.','0','=','*']
   ];
 
   //provides the value to be displayed.
-  //Only req'd to implement sci-notation for large numbers
-  //to stop the display <td> exceeding the calc width
-  $scope.theDisplay = function(){
-    if($scope.display > 1000000000){
-      $scope.display = $scope.display.toExponential(2);
-      return $scope.display;
+  $scope.theDisplay = function() {
+    //format the strings
+
+    if($scope.operand1 != '0' && $scope.operand2 != '0') {
+      return $scope.operand1;
+    } else if($scope.operand1 === '0' && $scope.operand2 != '0'){
+      return $scope.operand2;
+    } else {
+       return $scope.operand1;
     }
-    //small number with a lot of decimal places?
-    // var tempString = $scope.display.toString();
-    // if(tempString.length > 9){
-    //   $scope.display = $scope.display.toPrecision(9);
-    // }
-    // return $scope.display;
   };
 
   //sets up operator conditions when operator selected
   $scope.operation = function(operator) {
     $scope.operator = operator;
-    $scope.operand = $scope.display;
-    $scope.display = 0;
+    $scope.operand2 = $scope.operand1;
+    $scope.operand1 = '0'
     $scope.decPoint = false;
-    $scope.postDecPresses = 0;
+    $scope.minus = false;
   }; //operation
 
   //call to reset the calc. Pass true if you also want to reset the display, else pass false
   $scope.reset = function(display) {
-    if(display)
-      $scope.display = 0;
+    // if(display)
+    //   $scope.display = 0;
 
     $scope.operator = '';
-    $scope.operand = 0;
+    $scope.operand1 = '0';
+    $scope.operand2 = '0';
+    $scope.minus = false;
     $scope.decPoint = false;
-    $scope.postDecPresses = 0;
   }
+
+  $scope.negNum = function() {
+    if(!$scope.minus) { //add the - sign. No -ve zero
+      if($scope.operand1 !== '0') {
+        $scope.operand1 = '-' + $scope.operand1;
+        $scope.minus = true;
+      }
+    } else { //remove the - sign
+      $scope.operand1 = $scope.operand1.substring(1);
+      $scope.minus = false;
+    }
+  };
 
   //resolves the equation
   $scope.resolve = function() {
@@ -76,18 +86,11 @@ calcApp.controller('calc-controller', function($scope){
   //called when the user pushes a button on the calc
   $scope.push = function(value) {
     //is value a number?
-    if(!isNaN(parseFloat(value))) {
-      if($scope.decPoint === false) {
-        // if(($scope.display === 0) && ($scope.operator === '-') && ($scope.operand === 0)) {
-        //   $scope.display = (($scope.display * 10) + value) * -1;
-        //   $scope.operator = '';
-        // } else {
-          $scope.display = ($scope.display * 10) + value;
-        // }
-      } else { //if decimal point pressed
-        $scope.postDecPresses++;
-        $scope.display += value * Math.pow(10, ($scope.postDecPresses * -1));
-      }
+    if(!isNaN(parseFloat(value)) || value === '.') {
+      if($scope.operand1 === '0' && value != '.')
+        $scope.operand1 = value;
+      else
+        $scope.operand1 = $scope.operand1.concat(value);
     } else { //if value is an operation, do maths logic
       if((value === '+') || (value === '-') || (value === '*') || (value === '/')) {
         $scope.operation(value);
@@ -103,8 +106,8 @@ calcApp.controller('calc-controller', function($scope){
         case 'C':
           $scope.reset(true);
           break;
-        case ';)':
-          $scope.display = 7734;
+        case '±':
+          $scope.negNum();
           break;
         case ':)':
           $scope.display = 71077345;
